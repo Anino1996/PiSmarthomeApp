7427a227c202d59f32eefffcb6bb04520634a490
anino1996 <anino1996.ao@gmail.com>
2021-03-20 04:49:18 -0400

    secure and updated wth css and manual login wrapper

--- appServer/views.py
+++ appServer/views.py
@@ -9,14 +9,31 @@ from django.contrib.auth import authenticate, login, logout
 lightPin=smartPin(4)
 # Create your views here.
 
-class SwitchView(View):
-	def get(self, request):
-
+# Decorator to check if user is logged in
+def loginwrap(func):
+	def  wrapper_func(request):
 		if not request.user.is_authenticated:
 			return redirect('login')
 
+		return func(request)
+
+	return wrapper_func
+
+
+class SwitchView(View): #Main page to switch device on and off
+
+# Wrap page  in authentication decorator
+
+	@loginwrap
+	def get(self, request):
+
+# Get command passed by pressing button
 		command=request.GET.get('cmd')
+
+# Check device  status 
 		fdbck='Light on' if lightPin.is_active else 'Light off'
+
+# Turn device on or off based on command
 		if command:
 			if command.lower()=='on':
 				lightPin.switch_on()	
@@ -29,12 +46,19 @@ class SwitchView(View):
 		ctx={'cmd':fdbck}
 		return render(request,'appServer/home.html',ctx)
 
+
+# Handles manual login using authenticate
+
 class logView(View):
 
+
 	def get(self, request):
+
+# Create empty form and pass as context 
 		form=LoginForm()
 		ctx={'form':form}
-		return render(request, 'manual_login.html', ctx)
+		return render(request, 'appServer/manual_login.html', ctx)
+
 
 	def post(self, request):
 		recv=request.POST
